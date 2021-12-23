c6fd46cbb94f313ee5095c200e281de49f64d9e2
Dimas Fendriansyah <dimasfendriansyah123098@gmail.com>
2021-12-23 14:31:12 +0700

    Animation

--- assets/script/script.js
+++ assets/script/script.js
@@ -85,6 +85,96 @@ window.addEventListener('resize', () => {
     slider.style.transform = `translateX(${-(moveX * counter)}px)`;
 });
 
+// SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+// Animasi Home
+sr.reveal('.home-text', {})
+sr.reveal('#hero button', {delay: 200})
+sr.reveal('#hero img', {origin: 'right', delay: 1000})
+
+// Animasi About
+sr.reveal('#about img', {delay: 500})
+sr.reveal('#about p', {delay: 300})
+sr.reveal('#about h1', {delay: 400})
+
+// Animasi Skills
+sr.reveal('#feature h1', {})
+sr.reveal('#feature p', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('#feature figcaption', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('#feature img', {delay: 400})
+
+sr.reveal('#contact h1', {})
+sr.reveal('#contact button', {delay: 400})
+sr.reveal('input', {delay: 200})
+sr.reveal('textarea', {delay: 300})
+
+
+// Animasi Contact
+sr.reveal('#profile img', {})
+sr.reveal('#profile h3', {})
+
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #814BD7 }";
+    document.body.appendChild(css);
+  };
 
 
 
